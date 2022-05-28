67b9d7fdcc79a83c4e62499f23e3ca06b1f1a7c3
inzarubin <inzarubin80gmail.com>
2022-05-28 18:32:50 +0300

    est-schools

---
+++ server/controllers/schoolControllers.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-contacts/models"
+	u "go-contacts/utils"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+var CreateSchool = func(w http.ResponseWriter, r *http.Request) {
+	school := &models.School{}
+	err := json.NewDecoder(r.Body).Decode(school)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+	resp := school.CreateSchool()
+	u.Respond(w, resp)
+}
+
+var UpdateSchool = func(w http.ResponseWriter, r *http.Request) {
+	school := &models.School{}
+	err := json.NewDecoder(r.Body).Decode(school)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+	resp := school.UpdateSchool()
+	u.Respond(w, resp)
+}
+
+var GetSchools = func(w http.ResponseWriter, r *http.Request) {
+
+	data := models.GetSchools();
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
+
+var DeleteSchool = func(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	resp := models.DeleteSchool(id);
+	u.Respond(w, resp)
+}
+
+var GetSchool = func(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	data := models.GetSchool(id);
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
+
