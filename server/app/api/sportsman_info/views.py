8c9dc80dbcbc1db5738e7e676853bfa250af6f5f
Hariuc Piotr <acvatoris@gmail.com>
2022-11-08 00:23:55 +0300

    +

--- server/app/api/sportsman_info/views.py
+++ server/app/api/sportsman_info/views.py
@@ -18,22 +18,22 @@ class SportsmanInfoViewSet(viewsets.ModelViewSet):
         queryset = SportsmanInfoModel.objects.all()
 
         serializer = SportsmanInfoSerializers(queryset, many=True)
-        return Response({"status": "OK", "data": serializer.data})
+        return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"})
 
     def create(self, request, *args, **kwargs):
         serializer = SportsmanInfoCreateSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_200_OK)
         else:
-            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None, *args, **kwargs) -> Response:
 
         queryset = SportsmanInfoModel.objects.all()
         element = get_object_or_404(queryset, pk=pk)
         serializer = SportsmanInfoSerializers(element)
-        return Response({"status": "ok", "data": serializer.data})
+        return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"})
 
     def update(self, request, pk=None, *args, **kwargs) -> Response:
 
@@ -41,11 +41,11 @@ class SportsmanInfoViewSet(viewsets.ModelViewSet):
         serializer = SportsmanInfoCreateSerializers(element, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "OK", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_200_OK)
         else:
-            return Response({"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status": False}, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None, *args, **kwargs) -> Response:
         element = get_object_or_404(SportsmanInfoModel.objects.all(), pk=pk)
         element.delete()
-        return Response({"message": 'OK'})
+        return Response({"message": True})
--- server/app/api/tournament/views.py
+++ server/app/api/tournament/views.py
@@ -20,22 +20,22 @@ class TournamentViewSet(viewsets.ModelViewSet):
         if search_name is not None:
             queryset = queryset.filter(name__icontains=search_name)
         serializer = TournamentSerializers(queryset, many=True)
-        return Response({"status": "OK", "data": serializer.data})
+        return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"})
 
     def create(self, request, *args, **kwargs):
         serializer = TournamentCreateSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_200_OK)
         else:
-            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None, *args, **kwargs) -> Response:
 
         queryset = TournamentModel.objects.all()
         element = get_object_or_404(queryset, pk=pk)
         serializer = TournamentSerializers(element)
-        return Response({"status": "ok", "data": serializer.data})
+        return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"})
 
     def update(self, request, pk=None, *args, **kwargs) -> Response:
 
@@ -43,11 +43,11 @@ class TournamentViewSet(viewsets.ModelViewSet):
         serializer = TournamentCreateSerializers(element, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "OK", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_200_OK)
         else:
-            return Response({"status": "error"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status": False}, headers={"Access-Control-Allow-Origin": "*"}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None, *args, **kwargs) -> Response:
         element = get_object_or_404(TournamentModel.objects.all(), pk=pk)
         element.delete()
-        return Response({"message": 'OK'})
+        return Response({"message": True})
