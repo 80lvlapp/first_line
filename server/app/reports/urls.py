df9189d9439298f60b69c09c5787953a2a9f17f6
Hariuc Piotr <acvatoris@gmail.com>
2022-11-11 18:50:26 +0300

    +

--- server/app/reports/urls.py
+++ server/app/reports/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SportsmanPointsReportViewSet, TournamentsSportsmenReportViewSet
+from .views import SportsmanPointsReportViewSet, TournamentsSportsmenCategoryReportViewSet, TournamentsSportsmenReportViewSet
 
 
 urlpatterns = [
@@ -9,4 +9,7 @@ urlpatterns = [
 
     path("api/v1/reports/tournaments-sportsman-report",
          TournamentsSportsmenReportViewSet.as_view({'get': "list"})),
+
+     path("api/v1/reports/tournaments-sportsman-category-report",
+          TournamentsSportsmenCategoryReportViewSet.as_view({'get': "list"})),    
 ]
--- server/app/reports/views.py
+++ server/app/reports/views.py
@@ -12,6 +12,7 @@ from django.db.models import Sum
 
 from api.sportsman.sportsman_serializers import SportsmanSerializers
 from api.tournament.tournament_serializers import TournamentSerializers
+from api.category_value.category_value_serializers import CategoryValueCreateSerializer
 
 
 class SportsmanPointsReportViewSet(viewsets.ModelViewSet):
@@ -97,4 +98,60 @@ class TournamentsSportsmenReportViewSet(viewsets.ModelViewSet):
             json_qs.append(json_element)
             n += 1
         return JsonResponse(json_qs, safe = False , headers={"Access-Control-Allow-Origin": "*"})
+
+
+class TournamentsSportsmenCategoryReportViewSet(viewsets.ModelViewSet):
+    def list(self, request: Request, *args, **kwargs) -> JsonResponse:
+        print(f"{request.query_params}")
+        start_date = request.query_params.get('startDate')
+        end_date = request.query_params.get('endDate')
+        sportsman_id = request.query_params.get("sportsman_id")
+        tournament_id = request.query_params.get("tournament_id")
+        queryset = TournamentInfoModel.objects.all()
+        qs1 = TournamentInfoModel.objects.values("sportsman").annotate(points=Sum("points")).order_by("-points")
+        if start_date is not None:
+            if start_date != "":
+                queryset = queryset.filter(period__gte=start_date)
+        if end_date is not None:
+            if end_date != "":
+                queryset = queryset.filter(period__lte=end_date)
+
+        if sportsman_id is not None:
+            if sportsman_id != "":
+                sportsman_id = int(sportsman_id)
+                queryset = queryset.filter(sportsman__id__exact=sportsman_id)
+
+        if tournament_id is not None:
+            if tournament_id != "":
+                tournament_id = int(tournament_id)
+                queryset = queryset.filter(tournament__id__exact=tournament_id)
+
+        sportsman_set = set()
+        for item in list(qs1):
+            for item1 in list(queryset):
+                if(item1.sportsman.id == item["sportsman"]):
+                    sportsman_set.add(item1.sportsman) 
+
+        print(f"[TournamentsSportsmenCategoryReportViewSet]: ${sportsman_set}")
+        json_qs = []
+        n = 1 
+        for sportsman_item in list(sportsman_set):
+            sportsman_serializer = SportsmanSerializers(sportsman_item)
+            tournament_json = []
+            category_json = []
+            points = 0
+            for item in list(queryset):
+                if (sportsman_item.id == item.sportsman.id):
+                    tournament_serializer = TournamentSerializers(item.tournament)
+                    category_serializer = CategoryValueCreateSerializer(item.category_value)
+                    tournament_json.append({"tournament": tournament_serializer.data})
+                    category_json.append({"category": category_serializer.data, "point": item.point, "place": item.place})
+                    points += item.points
+            json_element = {"sportsman": sportsman_serializer.data,"place": n, "points": points, "tournaments": tournament_json, "categories": category_json}        
+            json_qs.append(json_element)
+            n += 1
+        return JsonResponse(json_qs, safe = False , headers={"Access-Control-Allow-Origin": "*"})
+
+
+
         
